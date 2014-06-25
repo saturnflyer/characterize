045208e251c9a86ade187a0dda52958a0171aee8
Jim Gay <jim@saturnflyer.com>
2014-06-24 22:10:42 -0400

    test rendered content

--- test/characterize_test.rb
+++ test/characterize_test.rb
@@ -4,5 +4,6 @@ describe UsersController do
   it 'renders views with extra features from characters' do
     get :show, id: '1'
     assert_template :show
+    assert_select 'a', 'm( O.O )m'
   end
 end
