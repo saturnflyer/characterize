3ec15c2e194b3529dfbd001c4a27c63e41f2f649
Jim Gay <jim@saturnflyer.com>
2022-07-18 10:41:11 -0400

    Generate test user

--- test/characterize_test.rb
+++ test/characterize_test.rb
@@ -2,13 +2,15 @@ require 'test_helper'
 
 describe UsersController do
   it 'renders views with extra features from default characters' do
-    get :show, params: { id: '1' }
+    user = User.create!(name: "Jim")
+    get :show, params: { id: user.id }
     assert_template :show
     assert_select 'a', 'm( O.O )m'
   end
 
   it 'renders objects with extra features from specified characters' do
-    get :edit, params: { id: '1' }
+    user = User.create!(name: "Jim")
+    get :edit, params: { id: user.id }
     assert_equal "Yay! Special title", response.body
   end
 end
