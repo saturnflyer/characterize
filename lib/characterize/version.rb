3600b6d02abee43dfdc9540ea59ea3437fd5d0cd
Jim Gay <jim@saturnflyer.com>
2023-01-07 13:08:45 -0500

    Add some notes and bump the minor version

--- lib/characterize/version.rb
+++ lib/characterize/version.rb
@@ -1,3 +1,3 @@
 module Characterize
-  VERSION = "0.1.0"
+  VERSION = "0.2.0"
 end
--- lib/characterize/view_forwards.rb
+++ lib/characterize/view_forwards.rb
@@ -3,14 +3,17 @@ module Characterize
   module ViewForwards
     extend Forwardable
   
+    # Find all view helper modules
     action_view_helpers = ActionView::Helpers::constants.map{|name|
       ActionView::Helpers.const_get(name) 
     }.select{|const|
       const.is_a?(Module)
     }
 
+    # Forward view helper module methods to the view object
     delegate [*action_view_helpers.map(&:instance_methods).flatten.uniq] => :__view__
 
+    # Forward URL helper module methods to the view object
     delegate [*Rails.application.routes.url_helpers.instance_methods] => :__view__
   end
 end
